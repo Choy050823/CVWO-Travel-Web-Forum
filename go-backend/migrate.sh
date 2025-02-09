540bd2a1a79070d6a4271ed3f0a521d9ebc40f39
ChoyMH23 <choymh23@gmail.com>
2025-02-09 19:14:07 +0800

    modify go backend to production state

--- go-backend/migrate.sh
+++ go-backend/migrate.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-chmod +x ./vendor/github.com/golang-migrate/migrate/v4/cmd/migrate
-./vendor/github.com/golang-migrate/migrate/v4/cmd/migrate -database "$DATABASE_URL" -path migrations up
+# chmod +x ./vendor/github.com/golang-migrate/migrate/v4/cmd/migrate
+# ./vendor/github.com/golang-migrate/migrate/v4/migrate.go -database "$DATABASE_URL" -path migrations up
+migrate -database "$DATABASE_URL" -path migrations up
 
 echo "Migrations complete."
