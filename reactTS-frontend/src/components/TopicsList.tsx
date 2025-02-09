b1a4e3199ffd33d4276e2dc612a19a48be56f88a
ChoyMH23 <choymh23@gmail.com>
2025-02-09 20:01:05 +0800

    modify code to production state

--- reactTS-frontend/src/components/TopicsList.tsx
+++ reactTS-frontend/src/components/TopicsList.tsx
@@ -1,38 +1,34 @@
-// import BASE_URL from "../config";
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Category } from "../models/models";
-import React from "react";
 
 const BASE_URL = import.meta.env.VITE_API_URL;
-const [topics, setTopics] = useState<Category[]>([]);
 
-const fetchCategories = async (): Promise<Category[]> => {
-  try {
-    const response = await fetch(`${BASE_URL}/api/categories`);
-    if (!response.ok) {
-      throw new Error("Failed to fetch categories");
+const TopicsList: React.FC = () => {
+  const [topics, setTopics] = useState<Category[]>([]);
+
+  const fetchCategories = async (): Promise<Category[]> => {
+    try {
+      const response = await fetch(`${BASE_URL}/api/categories`);
+      if (!response.ok) {
+        throw new Error("Failed to fetch categories");
+      }
+      const res = await response.json();
+      return res;
+    } catch (error) {
+      console.error("Error fetching categories:", error);
+      return [];
     }
-    const res = await response.json();
-    return res;
-  } catch (error) {
-    console.error("Error fetching categories:", error);
-    return [];
-  }
-};
-
-React.useEffect(() => {
-  const fetchData = async () => {
-    const res = await fetchCategories();
-    setTopics(res);
-    console.log(topics);
   };
-  fetchData();
-}, [])
 
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetchCategories();
+      setTopics(res);
+      console.log(topics);
+    };
+    fetchData();
+  }, []);
 
-
-
-const TopicsList = () => {
   return (
     <div className="bg-white p-4 rounded-lg shadow">
       <h3 className="font-semibold text-lg mb-4">Topics For You</h3>
